2ae2d12f0adc8be9711dda975d9add4f9d6dd006
audqls <audqls11@naver.com>
2018-06-21 18:03:58 +0900

    [채팅앱] 업데이트

--- ChatProj/app/src/main/java/com/example/gst/chatproj/ChatData.java
+++ ChatProj/app/src/main/java/com/example/gst/chatproj/ChatData.java
@@ -4,12 +4,20 @@ public class ChatData {
 
     private String userName;
     private String message;
+    private String firebaseKey;
+    private String userPotoUrl;
+    private String userEmail;
+    private long time;
 
     public ChatData() { }
 
-    public ChatData(String userName, String message) {
+    public ChatData(String userName, String message, String firebaseKey, String userPotoUrl, String userEmail, long time) {
         this.userName = userName;
         this.message = message;
+        this.firebaseKey = firebaseKey;
+        this.userPotoUrl = userPotoUrl;
+        this.userEmail = userEmail;
+        this.time = time;
     }
 
     public String getUserName() {
@@ -26,5 +34,38 @@ public class ChatData {
 
     public void setMessage(String message) {
         this.message = message;
+    }
+
+    public void setFirebaseKey(String firebaseKey) {
+        this.firebaseKey = firebaseKey;
+    }
+
+    public void setUserPotoUrl(String userPotoUrl) {
+        this.userPotoUrl = userPotoUrl;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+
+    public void setTime(long time) {
+        this.time = time;
+    }
+
+    public String getFirebaseKey() {
+
+        return firebaseKey;
+    }
+
+    public String getUserPotoUrl() {
+        return userPotoUrl;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public long getTime() {
+        return time;
     }
 }
--- ChatProj/app/src/main/java/com/example/gst/chatproj/MainActivity.java
+++ ChatProj/app/src/main/java/com/example/gst/chatproj/MainActivity.java
@@ -2,11 +2,13 @@ package com.example.gst.chatproj;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.text.TextUtils;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ListView;
 import android.widget.Toast;
 
 import com.google.firebase.database.ChildEventListener;
@@ -15,58 +17,88 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText editTxt;
     private Button sendBtn;
-    private ListView listView;
+    private RecyclerView listView;
 
-    private static String userName;
-    private ArrayAdapter adapter;
+    private String userName;
+    private ChatAdapter adapter;
 
     private FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
-    private DatabaseReference databaseReference = firebaseDatabase.getReference();
+    private DatabaseReference databaseReference = firebaseDatabase.getReference("message");
+
+    private ArrayList<ChatData> mItems = new ArrayList<>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        initViews();
+        initValues();
 
-        listView = (ListView) findViewById(R.id.listView);
-        editTxt = (EditText) findViewById(R.id.editText);
-        sendBtn = (Button) findViewById(R.id.sendBtn);
+        // 기본 Text를 담을 수 있는 simple_list_item_1을 사용해서 ArrayAdapter를 만들고 listview에 설정
+        adapter = new ChatAdapter(mItems);
 
-        userName = "user" + new Random().nextInt(10000);  // 랜덤한 유저 이름 설정 ex) user1234
-
-// 기본 Text를 담을 수 있는 simple_list_item_1을 사용해서 ArrayAdapter를 만들고 listview에 설정
-        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, android.R.id.text1);
         listView.setAdapter(adapter);
+        listView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+        listView.setItemAnimator(new DefaultItemAnimator());
+
 
         sendBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                ChatData chatData = new ChatData(userName, editTxt.getText().toString());
-                databaseReference.child("message").push().setValue(chatData);
-                editTxt.setText("");
+//                ChatData chatData = new ChatData(userName, editTxt.getText().toString());
+//                databaseReference.child("message").push().setValue(chatData);
+//                editTxt.setText("");
+                String message = editTxt.getText().toString();
+                if (!TextUtils.isEmpty(message)) {
+                    editTxt.setText("");
+                    ChatData chatData = new ChatData();
+                    chatData.setUserName(userName);
+                    chatData.setMessage(message);
+                    chatData.setTime(System.currentTimeMillis());
+                    databaseReference.child("message").push().setValue(chatData);
             }
-        });
+        }});
 
         databaseReference.child("message").addChildEventListener(new ChildEventListener() {  // message는 child의 이벤트를 수신합니다.
             @Override
             public void onChildAdded(DataSnapshot dataSnapshot, String s) {
                 ChatData chatData = dataSnapshot.getValue(ChatData.class);  // chatData를 가져오고
-                adapter.add(chatData.getUserName() + ": " + chatData.getMessage());  // adapter에 추가합니다.
-                Toast.makeText(MainActivity.this,""+chatData.getMessage(),Toast.LENGTH_SHORT).show();
+//                adapter.add(chatData.getUserName() + ": " + chatData.getMessage());  // adapter에 추가합니다.
+
+                //ChatData chatData = dataSnapshot.getValue(ChatData.class);
+                chatData.setFirebaseKey(dataSnapshot.getKey());
+                mItems.add(chatData);
+
+                adapter.notifyData(mItems);
+                listView.smoothScrollToPosition(adapter.getItemCount());
+               // adapter.notifyDataSetChanged();
+
+//                Toast.makeText(MainActivity.this,""+chatData.getMessage()+"//"+adapter.getItemCount(),Toast.LENGTH_SHORT).show();
             }
 
             @Override
             public void onChildChanged(DataSnapshot dataSnapshot, String s) { }
 
             @Override
-            public void onChildRemoved(DataSnapshot dataSnapshot) { }
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+//                String firebaseKey = dataSnapshot.getKey();
+//                int count = adapter.getCount();
+//                for (int i = 0; i < count; i++) {
+//                    if (adapter.get(i).equals(firebaseKey)) {
+//                        adapter.remove(adapter.getItem(i));
+//                        break;
+//                    }
+//                }
+            }
 
             @Override
             public void onChildMoved(DataSnapshot dataSnapshot, String s) { }
@@ -75,4 +107,18 @@ public class MainActivity extends AppCompatActivity {
             public void onCancelled(DatabaseError databaseError) { }
         });
     }
+
+    private void initViews() {
+        listView = (RecyclerView) findViewById(R.id.listView);
+        editTxt = (EditText) findViewById(R.id.editText);
+        sendBtn = (Button) findViewById(R.id.sendBtn);
+
+        editTxt = (EditText) findViewById(R.id.editText);
+        //findViewById(R.id.sendBtn).setOnClickListener(this);
+    }
+
+    private void initValues() {
+        userName = "Guest" + new Random().nextInt(5000);
+    }
+
 }
